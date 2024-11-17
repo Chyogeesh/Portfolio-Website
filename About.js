f00e69ea63a2d1e5f48d84ef97c59053de24d765
Ch Yogeesh Sai <108213999+Chyogeesh@users.noreply.github.com>
2024-11-17 21:17:29 +0530

    Create About.js

---
+++ About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './About.css';
+
+const About = () => {
+  return (
+    <section className="about">
+      <h2>About Me</h2>
+      <p>
+        I'm a passionate developer with experience in creating web applications using React, Node.js, and more.
+      </p>
+    </section>
+  );
+};
+
+export default About;
