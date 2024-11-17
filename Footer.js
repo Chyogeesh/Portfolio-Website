ad223cc06b03736c5c2445826dc134e1493238eb
Ch Yogeesh Sai <108213999+Chyogeesh@users.noreply.github.com>
2024-11-17 21:19:47 +0530

    Create Footer.js

---
+++ Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Contact: myemail@example.com</p>
+    </footer>
+  );
+};
+
+export default Footer;
